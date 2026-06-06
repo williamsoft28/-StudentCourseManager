62dbd67dc5476e1132601095962b628da63a8380
williamsoft28 <wilizbga28@gmail.com>
2026-06-06 21:52:17 +0200

    ALL

--- Code_Source/includes/auth.php
+++ Code_Source/includes/auth.php
@@ -1,8 +1,10 @@
 <?php
+// ici nous essayons de verifier si la session est dejà demarree
 if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
 
+//ici nous essayons de verifier si l'utilisateur est connecté en tant qu'étudiant
 if (!isset($_SESSION['student_id'])) {
     header('Location: ../pages/login.php');
     exit();
